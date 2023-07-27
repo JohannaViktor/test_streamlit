74aec26c06427fdc4e68fa9ed5be73b53fd4f955
JohannaViktor <54990684+JohannaViktor@users.noreply.github.com>
2023-07-27 13:24:18 +0200

    Add files via upload

---
+++ test_pipenv_app/test.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+# importing customer churn data
+data = pd.read_csv("./data/training_data.csv", index_col=0)
+st.write("Churn Data")
+st.write(data)
+
+st.write("How many customers in the dataset churned?")
+target_bins = data.loc[:, 'Churn'].value_counts()
+st.bar_chart(target_bins)
